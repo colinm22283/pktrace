870b9cf0ca055afa2c1dc77a9ebe30bea0708680
Colin M <colinm22283@gmail.com>
2022-08-18 21:35:52 -0500

    Added more color stuff

--- source/render/color.cpp
+++ source/render/color.cpp
@@ -11,3 +11,13 @@ color color::operator+(color c)
         (uint8_t)CLAMP(a + c.a, 0, 255)
     );
 }
+
+color color::operator-(color c)
+{
+    return RGBA(
+            (uint8_t)CLAMP(r - c.r, 0, 255),
+            (uint8_t)CLAMP(g - c.g, 0, 255),
+            (uint8_t)CLAMP(b - c.b, 0, 255),
+            (uint8_t)CLAMP(a - c.a, 0, 255)
+    );
+}
